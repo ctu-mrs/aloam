488f27b1d0fc0503e4f3b6893c434cd22eb0f2e6
uav3 <uav3@mrs.felk.cvut.cz>
2019-09-18 14:16:26 +0200

    auto yes during install

--- install/install.sh
+++ install/install.sh
@@ -4,22 +4,22 @@
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # CMake
-sudo apt-get install cmake
+sudo apt-get install --yes cmake
 
 # google-glog + gflags
-sudo apt-get install libgoogle-glog-dev
+sudo apt-get install --yes libgoogle-glog-dev
 
 # BLAS & LAPACK
-sudo apt-get install libatlas-base-dev
+sudo apt-get install --yes libatlas-base-dev
 
 # Eigen3
-sudo apt-get install libeigen3-dev
+sudo apt-get install --yes libeigen3-dev
 
 # SuiteSparse and CXSparse (optional)
 # - If you want to build Ceres as a *static* library (the default)
 #   you can use the SuiteSparse package in the main Ubuntu package
 #   repository:
-sudo apt-get install libsuitesparse-dev
+sudo apt-get install --yes libsuitesparse-dev
 
 # download ceres solver
 cd $SCRIPT_PATH
@@ -35,9 +35,12 @@ mkdir ceres-bin
 cd ceres-bin
 cmake ../ceres-solver-1.14.0
 make -j3
-make install
+sudo make install
 
 # clean tmp files
 cd $SCRIPT_PATH
 rm -rf tmp
 
+# build catkin package
+cd $SCRIPT_PATH
+catkin build aloam
