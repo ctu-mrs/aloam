352139aab1fdf0b9880b0ee6c1b9542a1f3aaa1c
Pavel Petracek <petrapa6@fel.cvut.cz>
2021-02-18 13:50:47 +0100

    merge ee22d9a4

--- include/aloam_slam/mapping.h
+++ include/aloam_slam/mapping.h
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <mutex>
 #include <queue>
+#include <condition_variable>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -87,6 +88,7 @@ private:
 
   // Feature extractor newest data
   std::mutex                        _mutex_odometry_data;
+  std::condition_variable           _cv_odometry_data;
   bool                              _has_new_data = false;
   ros::Time                         _time_aloam_odometry;
   tf::Transform                     _aloam_odometry;
