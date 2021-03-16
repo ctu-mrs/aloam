809751faeb04d595a338297485fda2291691d78f
Pavel Petracek <petrapa6@fel.cvut.cz>
2021-03-16 12:39:57 +0100

    improve installation process

--- install/install.sh
+++ install/install.sh
@@ -2,52 +2,44 @@
 
 # get path to script
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
 distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
 
-# CMake
-sudo apt-get install --yes cmake
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+CERES_PATH=$SCRIPT_PATH/../lib
+CERES_VERSION=1.14.0
 
-# google-glog + gflags
-sudo apt-get install --yes libgoogle-glog-dev
+# install dependencies
+sudo apt update
+sudo apt-get -y install \
+  cmake \
+  libgoogle-glog-dev \
+  libatlas-base-dev \
+  libeigen3-dev \
+  libsuitesparse-dev \
+  ros-$ROS_DISTRO-pcl-ros \
+  ros-$ROS_DISTRO-pcl-conversions \
+  ros-$ROS_DISTRO-pcl-msgs
 
-# BLAS & LAPACK
-sudo apt-get install --yes libatlas-base-dev
-
-# Eigen3
-sudo apt-get install --yes libeigen3-dev
-
-# SuiteSparse and CXSparse (optional)
-# - If you want to build Ceres as a *static* library (the default)
-#   you can use the SuiteSparse package in the main Ubuntu package
-#   repository:
-sudo apt-get install --yes libsuitesparse-dev
- 
-# PCL
-if [ "$distro" = "18.04" ]; then
-  sudo apt-get install --yes ros-melodic-pcl-ros ros-melodic-pcl-conversions ros-melodic-pcl-msgs
-elif [ "$distro" = "20.04" ]; then
-  sudo apt-get install --yes ros-noetic-pcl-ros ros-noetic-pcl-conversions ros-noetic-pcl-msgs
-else
-  echo -e "\e[31mUbuntu version not 18.04 or 20.04, installing Noetic packages.\e[0m"
-  sudo apt-get install --yes ros-noetic-pcl-ros ros-noetic-pcl-conversions ros-noetic-pcl-msgs
-fi
 
 # download ceres solver
-cd $SCRIPT_PATH
-mkdir tmp
-cd tmp
-wget http://ceres-solver.org/ceres-solver-1.14.0.tar.gz
+[ ! -d $CERES_PATH ] && mkdir -p $CERES_PATH
+cd $CERES_PATH
 
-# unpack source files
-tar zxf ceres-solver-1.14.0.tar.gz
+if [ ! -d $CERES_PATH/ceres-solver-$CERES_VERSION ]
+then
+  # unpack source files
+  wget -O $CERES_PATH/ceres-solver-$CERES_VERSION.tar.gz http://ceres-solver.org/ceres-solver-$CERES_VERSION.tar.gz
+  tar zxf ceres-solver-$CERES_VERSION.tar.gz
+  rm -f ceres-solver-$CERES_VERSION.tar.gz
+fi
 
 # install ceres solver
-mkdir ceres-bin
-cd ceres-bin
-cmake ../ceres-solver-1.14.0
-make -j3
+cd $CERES_PATH/ceres-solver-$CERES_VERSION
+[ ! -d "build" ] && mkdir build
+cd build
+cmake ..
+make -j$[$(nproc) - 1]
 sudo make install
-
-# clean tmp files
-cd $SCRIPT_PATH
-rm -rf tmp
